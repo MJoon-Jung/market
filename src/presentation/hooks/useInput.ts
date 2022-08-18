ab622c3da3fc6747dbc59c86cc76cc2d19fb976e
MyeongJoon Jung <gjgjajaj31@gmail.com>
2022-08-18 17:15:47 +0900

    Delete src/presentation/hooks directory

--- src/presentation/hooks/useInput.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-function useInput(initalValue: any) {
-  const [value, setValue] = useState(initalValue);
-  return [value, setValue];
-}
-
-export default useInput;
